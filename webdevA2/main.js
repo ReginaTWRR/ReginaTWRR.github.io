759c294c9083d8a6378810612e9e9218e02a2596
Regero <107737505+ReginaTWRR@users.noreply.github.com>
2025-08-04 15:05:39 +0800

    Update main.js

--- webdevA2/main.js
+++ webdevA2/main.js
@@ -82,7 +82,7 @@ const thinkingSound = new Audio("./audio/thinkingSound.m4a");
 const thinker = document.querySelector("#thinker");
 thinker.addEventListener("click", function() {
     thinkingSound.play();
-})
+});
 /* js for philosopher profiles */
 var dynamicArea = document.querySelector("#dynamicArea");
 const addPhilosopher = document.querySelector("#addPhilosopher");
@@ -146,12 +146,20 @@ const noOfQuestions = 10;
 const answers = ["knowledge", "wonder", "rightness", "consequentialism", "action",
     "virtues", "meaning", "facts", "nihilism", "relativism"];
 var score = 0;
+var wrongAnswers = [];
+var removedInputs = [];
+var inputParents = [];
 submitButton.addEventListener("click", function() {
     score = 0;
     for (var i = 1; i < noOfQuestions + 1; ++i) {
         checkAnswer(i, answers[i - 1]);
     }
     scoreBox.innerHTML = "Score: " + score;
+    for (i = 0; i < wrongAnswers.length; ++i) {
+        inputParents.push(wrongAnswers[i].parentElement);
+        wrongAnswers[i].remove();
+        removedInputs.push(wrongAnswers[i]);
+    }
 });
 resetButton.addEventListener("click", function() {
     score = 0;
@@ -162,11 +170,18 @@ resetButton.addEventListener("click", function() {
             selectedValue.checked = false;
         }
     }
+    for (i = 0; i < wrongAnswers.length; ++i) {
+        // inserts the removed input (radio button) back to the left of the label
+        inputParents[i].insertBefore(removedInputs[i], inputParents[i].firstChild);
+    }
 });
 function checkAnswer(questionNumber, correctAnswer) {
     var selectedValue = document.querySelector("input[name='q" + questionNumber + "']:checked").value;
     if (selectedValue == correctAnswer) {
         ++score;
+    } else {
+        var selectedInput = document.querySelector("input[name='q" + questionNumber + "']:checked");
+        wrongAnswers.push(selectedInput); // .push() appends to an array
     }
 }
 /* js for mini game */
@@ -190,27 +205,48 @@ function showView(viewNumber) {
 }
 button1.addEventListener("click", function() {
     showView(1);
-    resetTrolley();
+    resetTrolley(1);
 });
 button2.addEventListener("click", function() {
     showView(2);
+    resetTrolley(2);
+    resetMan(1);
 });
 button3.addEventListener("click", function() {
     showView(3);
+    resetTrolley(3);
+    resetMan(2);
 });
-hideAllViews;
+hideAllViews();
 showView(1);
 // the trolley problem
-spawnDivs("tracks", 1, 9, 1);
+var mobileLayout;
+const qrcode = document.querySelector("#qrcode");
+// computed style is the style used on the element after all styling sources have been applied
+const qrcodeDisplay = window.getComputedStyle(qrcode).display;
+if (qrcodeDisplay == "none") {
+    mobileLayout = true;
+} else {
+    mobileLayout = false;
+}
+spawnDivs("tracks", 1, 11, 1);
 spawnDivs("innocent", 1, 7, 1);
 var currentTrolley;
+const trolley1 = document.querySelector("#trolley1");
 const startButton1 = document.querySelector("#startButton1");
 const switchButton = document.querySelector("#switchButton");
 const resetButton1 = document.querySelector("#resetButton1");
 var trolleyItvId;
 clearInterval(trolleyItvId);
-var leftInc = 375;
-var topInc = 1000;
+var leftInc;
+var topInc;
+if (mobileLayout == true) {
+    leftInc = 12.5;
+    topInc = 1600;
+} else {
+    leftInc = 375;
+    topInc = 1080;
+}
 var div1 = document.createElement("div");
 view1.appendChild(div1);
 startButton1.addEventListener("click", function () {
@@ -219,25 +255,38 @@ startButton1.addEventListener("click", function () {
     }, 1000);
 });
 switchButton.addEventListener("click", function() {
-    if (leftInc < 975) {
-        leftInc = 675;
-        topInc = 800;
-        trolley1.style.left = leftInc + "px";
-        trolley1.style.top = topInc + "px";
+    if (mobileLayout == true) {
+        if (leftInc < 212.5) {
+            leftInc = 162.5;
+            topInc = 1500;
+            trolley1.style.left = leftInc + "px";
+            trolley1.style.top = topInc + "px";
+        }
+    } else {
+        if (leftInc < 775) {
+            leftInc = 675;
+            topInc = 900;
+            trolley1.style.left = leftInc + "px";
+            trolley1.style.top = topInc + "px";
+        }
     }
 });
 resetButton1.addEventListener("click", function() {
     resetTrolley(1);
 });
 // the fat man
-spawnDivs("tracks", 9, 14, 2);
+spawnDivs("tracks", 11, 16, 2);
 spawnDivs("innocent", 7, 12, 2);
 var currentMan;
 const startButton2 = document.querySelector("#startButton2");
 const pushButton1 = document.querySelector("#pushButton1");
 const resetButton2 = document.querySelector("#resetButton2");
-const fatMan = document.querySelector("#fatMan");
 var topMargin;
+if (mobileLayout == true) {
+    topMargin = 1600;
+} else {
+    topMargin = 1080;
+}
 var div2 = document.createElement("div");
 view2.appendChild(div2);
 startButton2.addEventListener("click", function() {
@@ -246,8 +295,14 @@ startButton2.addEventListener("click", function() {
     }, 1000);
 });
 pushButton1.addEventListener("click", function() {
-    if (leftInc < 575) {
-        pushMan(1);
+    if (mobileLayout == true) {
+        if (leftInc < 112.5) {
+            pushMan(1);
+        }
+    } else {
+        if (leftInc < 575) {
+            pushMan(1);
+        } 
     }
 });
 resetButton2.addEventListener("click", function() {
@@ -255,12 +310,11 @@ resetButton2.addEventListener("click", function() {
     resetMan(1);
 });
 // the fat villain
-spawnDivs("tracks", 14, 19, 3);
+spawnDivs("tracks", 16, 21, 3);
 spawnDivs("innocent", 12, 17, 3);
 const startButton3 = document.querySelector("#startButton3");
 const pushButton2 = document.querySelector("#pushButton2");
 const resetButton3 = document.querySelector("#resetButton3");
-const fatVillain = document.querySelector("#fatVillain");
 var div3 = document.createElement("div");
 view3.appendChild(div3);
 startButton3.addEventListener("click", function() {
@@ -269,14 +323,20 @@ startButton3.addEventListener("click", function() {
     }, 1000);
 });
 pushButton2.addEventListener("click", function() {
-    if (leftInc < 575) {
-        pushMan(2);
+    if (mobileLayout == true) {
+        if (leftInc < 112.5) {
+            pushMan(2);
+        }
+    } else {
+        if (leftInc < 575) {
+            pushMan(2);
+        }
     }
-})
+});
 resetButton3.addEventListener("click", function() {
     resetTrolley(3);
     resetMan(2);
-})
+});
 function spawnDivs(name, start, end, view) {
     var currentView;
     switch (view) {
@@ -293,7 +353,7 @@ function spawnDivs(name, start, end, view) {
             break;
     }
     for (var i = start; i < end; ++i) {
-        newDiv = document.createElement("div");
+        var newDiv = document.createElement("div");
         newDiv.id = name + i;
         newDiv.className = name;
         currentView.appendChild(newDiv);
@@ -309,25 +369,49 @@ function moveTrolley(trolleyNumber) {
     } else if (trolleyNumber == 3) {
         currentDiv = div3;
     }
-    if (topMargin == 1050) {
-        if (leftInc > 375) {
-            clearInterval(trolleyItvId);
-            if (trolleyNumber == 2) {
-                div2.textContent = "I can't believe you would do this.";
-            } else if (trolleyNumber == 3) {
-                div3.textContent = "He deserves death because he's a villain?";
+    if (mobileLayout == true) {
+        if (topMargin == 1625) {
+            if (leftInc > 12.5) {
+                clearInterval(trolleyItvId);
+                if (trolleyNumber == 2) {
+                    div2.textContent = "I can't believe you would do this.";
+                } else if (trolleyNumber == 3) {
+                    div3.textContent = "He deserves death because he's a villain?";
+                }
             }
         }
-    }
-    if (leftInc < 975) {
-        leftInc += 100;
-        currentTrolley.style.left = leftInc + "px";
+        if (leftInc < 312.5) {
+            leftInc += 50;
+            currentTrolley.style.left = leftInc + "px";
+        } else {
+            clearInterval(trolleyItvId);
+            if (topInc == 1600) {
+                currentDiv.textContent = "Inaction is a choice too, you know.";
+            } else if (trolleyNumber == 1) {
+                div1.textContent = "Are human lives just numbers to you?";
+            }
+        }
     } else {
-        clearInterval(trolleyItvId);
-        if (topInc == 1000) {
-            currentDiv.textContent = "Inaction is a choice too, you know.";
-        } else if (trolleyNumber == 1) {
-            div1.textContent = "Are human lives just numbers to you?";
+        if (topMargin == 1130) {
+            if (leftInc > 375) {
+                clearInterval(trolleyItvId);
+                if (trolleyNumber == 2) {
+                    div2.textContent = "I can't believe you would do this.";
+                } else if (trolleyNumber == 3) {
+                    div3.textContent = "He deserves death because he's a villain?";
+                }
+            }
+        }
+        if (leftInc < 975) {
+            leftInc += 100;
+            currentTrolley.style.left = leftInc + "px";
+        } else {
+            clearInterval(trolleyItvId);
+            if (topInc == 1080) {
+                currentDiv.textContent = "Inaction is a choice too, you know.";
+            } else if (trolleyNumber == 1) {
+                div1.textContent = "Are human lives just numbers to you?";
+            }
         }
     }
 }
@@ -343,13 +427,22 @@ function resetTrolley(trolleyNumber) {
         currentDiv = div3;
     }
     currentDiv.textContent = "";
-    leftInc = 375;
-    topInc = 1000;
+    if (mobileLayout == true) {
+        leftInc = 12.5;
+        topInc = 1600;
+    } else {
+        leftInc = 375;
+        topInc = 1080;
+    }
     currentTrolley.style.left = leftInc + "px";
     currentTrolley.style.top = topInc + "px";
 }
 function pushMan(manNumber) {
-    topMargin = 1050;
+    if (mobileLayout == true) {
+        topMargin += 25;
+    } else {
+        topMargin += 50;
+    }
     if (manNumber == 1) {
         currentMan = document.querySelector("#fatMan");
     } else if (manNumber == 2) {
@@ -358,7 +451,11 @@ function pushMan(manNumber) {
     currentMan.style.top = topMargin + "px";
 }
 function resetMan(manNumber) {
-    topMargin = 1000;
+    if (mobileLayout == true) {
+        topMargin = 1600;
+    } else {
+        topMargin = 1080;
+    }
     if (manNumber == 1) {
         currentMan = document.querySelector("#fatMan");
     } else if (manNumber == 2) {
@@ -369,8 +466,16 @@ function resetMan(manNumber) {
 /* js for full screen */
 const fullScreenButton = document.querySelector("#fullScreenButton");
 const exitScreenButton = document.querySelector("#exitScreenButton");
-fullScreenButton.addEventListener("click", enterFullScreen);
-exitScreenButton.addEventListener("click", exitFullScreen);
+fullScreenButton.addEventListener("click", function() {
+    enterFullScreen();
+    fullScreenButton.classList.add("hideButton");
+    exitScreenButton.classList.remove("hideButton");
+});
+exitScreenButton.addEventListener("click", function() {
+    exitFullScreen();
+    fullScreenButton.classList.remove("hideButton");
+    exitScreenButton.classList.add("hideButton");
+});
 function enterFullScreen() {
     if (document.documentElement.requestFullscreen) {
         document.documentElement.requestFullscreen();
